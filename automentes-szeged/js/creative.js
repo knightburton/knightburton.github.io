d6153a04427ef7e18caf9a3a16d0212925beaeaf
Imre Kiss <kissi@inf.u-szeged.hu>
2016-10-17 15:40:45 +0200

    Improve the test site with some fancy stuff.
    Add some picture to the portfolio.
    Add some new keyword to the header meta.
    Change some text to look like nicer.

--- automentes-szeged/js/creative.js
+++ automentes-szeged/js/creative.js
@@ -159,3 +159,63 @@ function initMap() {
         map: map,
     });
 }
+
+jQuery(document).ready(function ($) {
+    var options = {
+        $AutoPlay: true,
+        $AutoPlaySteps: 1,
+        $Idle: 3000,
+        $PauseOnHover: 1,
+
+        $ArrowKeyNavigation: true,
+        $SlideEasing: $JssorEasing$.$EaseOutQuint,
+        $SlideDuration: 1500,
+        $MinDragOffsetToSlide: 20,
+        //$SlideWidth: 600,
+        //$SlideHeight: 300,
+        $SlideSpacing: 0,
+        $Cols: 1,
+        $ParkingPosition: 0,
+        $UISearchMode: 1,
+        $PlayOrientation: 1,
+        $DragOrientation: 1,
+
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$,
+            $ChanceToShow: 2,
+            $AutoCenter: 2,
+            $Steps: 1,
+            $Scale: false
+        },
+
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$,
+            $ChanceToShow: 2,
+            $AutoCenter: 1,
+            $Steps: 1,
+            $Rows: 1,
+            $SpacingX: 12,
+            $SpacingY: 4,
+            $Orientation: 1,
+            $Scale: false
+        }
+    };
+
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+    //responsive code begin
+    function ScaleSlider() {
+        var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+        if (parentWidth) {
+            jssor_slider1.$ScaleWidth(parentWidth - 30);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+});
