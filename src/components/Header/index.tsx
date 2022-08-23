385641afecb983308639eaa451dd4c8a6920192f
Imre Kiss <imre.kiss@jaystack.com>
2022-08-23 13:19:58 +0200

    Update the main header title.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,9 @@ const Header = () => (
     <div>
       <p className="m0 name">Imre Kiss</p>
       <p className="m0 position">
-        Software Developer at{' '}
-        <a href="https://jaystack.com/" target="_blank" rel="noreferrer">
-          JayStack
+        Senior Software Engineer at{' '}
+        <a href="https://www.one-beyond.com/" target="_blank" rel="noreferrer">
+          One Beyond
         </a>
       </p>
     </div>
